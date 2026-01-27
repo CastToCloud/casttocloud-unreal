db6c010b47cd26a3056bf29566705c8a7b1d67a1
Alexandru Oprea <oprea_alexandrum@yahoo.com>
2026-01-27 23:28:23 +0200

    Add missing DLL export to access main module

--- Source/CastToCloudAnalytics/Public/CtcAnalyticsModule.h
+++ Source/CastToCloudAnalytics/Public/CtcAnalyticsModule.h
@@ -6,7 +6,7 @@
 
 class FCtcAnalyticsProvider;
 
-class FCtcAnalyticsModule final : public IAnalyticsProviderModule
+class CASTTOCLOUD_API FCtcAnalyticsModule final : public IAnalyticsProviderModule
 {
 public:
 	static FCtcAnalyticsModule& Get();
